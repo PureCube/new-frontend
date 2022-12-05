313285da2e847123a8ddc8a1c728ce1ea63e5324
KIRA <bfakhruz@gmail.com>
2022-12-05 21:53:54 +0500

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,21 @@ module.exports = {
     "./features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      text: "#ffffff",
-      background: "#000000",
-    },
+    container: {
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+				'2xl': '6rem',
+			},
+		},
+    fontFamily: {
+			'heading-semibold': ['BeVietnamPro-SemiBold'],
+			'heading-medium': ['BeVietnamPro-Medium'],
+			'body-semibold': ['NeutrifPro-SemiBold'],
+			'body-regular': ['NeutrifPro-Regular'],
+		},
     extend: {},
   },
   plugins: [],
