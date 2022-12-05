793829519d18f6a2367a348636606de90d07c901
KIRA <bfakhruz@gmail.com>
2022-12-05 15:43:22 +0500

    added meta widget

---
+++ widgets/meta/index.tsx
@@ -0,0 +1,21 @@
+export const Meta = () => {
+	return (
+		<>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width" />
+			<meta name="description" content="PureCube is a next-generation gaming studio launching its games and developing a unified gaming platform for blockchain games" />
+			<meta name="author" content="PureCube" />
+			<meta property="og:url" content="https://purecube.io/" />
+			<meta property="og:type" content="website" />
+			<meta property="og:image" content="https://www.purecube.io/og-image.jpg?v3" />
+			<meta property="og:title" content="The rise of blockchain gaming | PureCube" />
+			<meta property="og:site_name" content="PureCube" />
+			<meta property="og:description" content="PureCube is a next-generation gaming studio launching its games and developing a unified gaming platform for blockchain games" />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@purecube_io" />
+			<meta name="og:image" content="https://www.purecube.io/og-image.jpg?v3" />
+			<meta name="twitter:title" content="The rise of blockchain gaming | PureCube" />
+			<meta name="twitter:description" content="PureCube is a next-generation gaming studio launching its games and developing a unified gaming platform for blockchain games" />
+		</>
+	)
+}
