3084a90d58905b3231327ef1cfdc8c936fefbff2
KIRA <bfakhruz@gmail.com>
2022-12-05 16:49:51 +0500

    created profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Container, Meta } from "shared";
+
+export default function ProfilePage() {
+  return (
+    <>
+      <Head>
+        <title>PureCube</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <Meta />
+      </Head>
+
+      <Container>
+        
+      </Container>
+    </>
+  )
+}
