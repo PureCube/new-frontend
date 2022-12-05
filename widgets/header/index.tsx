9e9fe392ee64950c4ae8e6fa3c8812f8cbd42930
KIRA <bfakhruz@gmail.com>
2022-12-05 20:42:12 +0500

    edited header widget

--- widgets/header/index.tsx
+++ widgets/header/index.tsx
@@ -1,5 +1,23 @@
-export const Header = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+export interface HeaderProps {
+	navbar: JSX.Element,
+}
+
+export const Header = ({navbar}: HeaderProps) => {
 	return (
-		<header>header</header>
+		<header className="bg-white">
+
+			<Link href="/">
+				<div className="relative h-12 w-20 md:w-24">
+					<Image src="/logo.svg" layout="fill" alt="logo" />
+				</div>
+			</Link>
+
+			{navbar}
+
+			
+		</header>
 	)
 }
