5d2b9660ccdcb933d4e4c059220be849f7b78f1c
KIRA <bfakhruz@gmail.com>
2022-12-05 21:58:04 +0500

    updated user-avatar ui component

--- entities/user/ui/user-avatar/index.tsx
+++ entities/user/ui/user-avatar/index.tsx
@@ -1,7 +1,15 @@
-export const UserAvatar = () => {
+import { userModel } from "entities/user";
+import { observer } from "mobx-react-lite";
+import Image from "next/image";
+
+export interface UserAvatarProps {
+	size: number,
+}
+
+export const UserAvatar = observer( ({size}: UserAvatarProps) => {
 	return (
-		<div>
-			user-avatar
+		<div className={`relative w-${size} h-${size}`}>
+			<Image src={userModel.userAvatar} layout="fill" alt="user avatar" />
 		</div>
 	)
-}
+} )
