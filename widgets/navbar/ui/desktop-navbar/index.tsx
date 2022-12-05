fe3a89e4c34e8b026e380930ae8c9e29f192cfcf
KIRA <bfakhruz@gmail.com>
2022-12-05 21:59:40 +0500

    edited desktop-navbar ui component

--- widgets/navbar/ui/desktop-navbar/index.tsx
+++ widgets/navbar/ui/desktop-navbar/index.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+
 export const DesktopNavbar = () => {
 	return (
-		<nav className="hidden lg:block">desktop navbar</nav>
+		<nav className="hidden lg:block">
+			<ul className="flex justify-center items-center space-x-10 font-body-semibold">
+
+				<li className="hover:underline">
+					<Link href="/games">Games</Link>
+				</li>
+
+				<li className="hover:underline">
+					<Link href="/about">About</Link>
+				</li>
+
+				<li className="hover:underline">
+					<Link href="/projects">Projects</Link>
+				</li>
+
+			</ul>
+		</nav>
 	)
 }
