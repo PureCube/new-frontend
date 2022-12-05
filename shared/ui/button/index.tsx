229aa1a553b7669206c0d41f6678bafc74e4397b
KIRA <bfakhruz@gmail.com>
2022-12-05 21:58:41 +0500

    edited shared/ui button component

--- shared/ui/button/index.tsx
+++ shared/ui/button/index.tsx
@@ -4,6 +4,6 @@ export interface ButtonProps {
 
 export const Button = ({children}: ButtonProps) => {
 	return (
-		<button>{children}</button>
+		<button className="font-heading-semibold text-sm md:text-base text-black bg-white rounded-full px-5 py-2 select-none">{children}</button>
 	)
 }
