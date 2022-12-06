02a080ac3d1d483269ff054537b7efe422928303
KIRA <bfakhruz@gmail.com>
2022-12-06 16:44:46 +0500

    updated home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import { Container, Main } from "shared/ui";
 import { Footer } from "widgets/footer";
 import { Header } from "widgets/header";
 import { Navbar } from "widgets/navbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, A11y, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { HomeHero } from "widgets/hero";
 
 export default function HomePage() {
   return (
@@ -18,7 +23,40 @@ export default function HomePage() {
         <Header
           navbar={<Navbar />}
         />
-        <Main>main</Main>
+        <Main>
+          <Swiper
+            modules={[Navigation, A11y, Autoplay]}
+            slidesPerView={1}
+            navigation
+            autoplay={{
+              delay: 7000,
+            }}
+          >
+
+            <SwiperSlide>
+              <HomeHero
+                title="Welcome to PureCube"
+                body="PureCube is a next-generation gaming studio launching its games and developing a unified gaming platform for blockchain games"
+                link="https://medium.com/@pureblock/welcome-to-pureblock-5a78cc85738d"
+                target="_blank"
+                image="/images/001.jpg"
+                imagePosition="right"
+              />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <HomeHero
+                title="First Block"
+                body="Discover the First Block ecosystem in the PureCube"
+                link="https://pureblock.medium.com/discover-the-first-block-ecosystem-in-the-pureblock-392a7778e9ef"
+                target="_blank"
+                image="/images/002.jpg"
+                imagePosition="bottom"
+              />
+            </SwiperSlide>
+
+          </Swiper>
+        </Main>
         <Footer />
       </Container>
     </>
