d55b3eba7f61d6fd573bbc908934523a16a77536
KIRA <bfakhruz@gmail.com>
2022-12-07 15:26:33 +0500

    edited home-hero ui component

--- widgets/hero/ui/home-hero/index.tsx
+++ widgets/hero/ui/home-hero/index.tsx
@@ -15,7 +15,7 @@ export const HomeHero = ({title, body, link, target="_self", image, imagePositio
 		<section className="relative overflow-hidden flex items-end h-[50vh] md:h-[80vh] bg-neutral-900 p-3 md:p-5 rounded-3xl">
 			{image && <Image src={image} layout="fill" objectFit="cover" objectPosition={imagePosition} alt="cover" />}
 			<div className="relative w-full space-y-3">
-				<h1 className="text-3xl md:text-4xl leading-normal font-heading-bold uppercase">{title}</h1>
+				<h1 className="text-3xl md:text-4xl leading-normal font-heading-semibold uppercase">{title}</h1>
 				<p className="text-base md:text-lg md:w-2/3">{body}</p>
 				<LinkButton link={link} target={target} arrow={true}>Read about</LinkButton>
 			</div>
