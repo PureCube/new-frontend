9e298cdc246bda6ec63344a2aa6c65adff41ded4
KIRA <bfakhruz@gmail.com>
2022-12-09 17:24:33 +0500

    updated home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import { InfoBlock } from "widgets/info-block";
 import { GamesCards } from "widgets/games";
 import { Partners } from "widgets/partners";
 import { NewsList } from "widgets/news-list";
+import { newsListModel } from "widgets/news-list/model";
 
 export default function HomePage() {
   return (
@@ -40,27 +41,22 @@ export default function HomePage() {
                 delay: 7000,
               }}
             >
-              <SwiperSlide>
-                <HomeHero
-                  title="Welcome to PureCube"
-                  body="PureCube is a next-generation gaming studio launching its games and developing a unified gaming platform for blockchain games"
-                  link="https://medium.com/@pureblock/welcome-to-pureblock-5a78cc85738d"
-                  target="_blank"
-                  image="/images/001.jpg"
-                  imagePosition="right"
-                />
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <HomeHero
-                  title="First Block"
-                  body="Discover the First Block ecosystem in the PureCube"
-                  link="https://pureblock.medium.com/discover-the-first-block-ecosystem-in-the-pureblock-392a7778e9ef"
-                  target="_blank"
-                  image="/images/002.jpg"
-                  imagePosition="bottom"
-                />
-              </SwiperSlide>
+              {
+                newsListModel.newsList.slice(-7).map ( (news, index) => {
+                  return (
+                    <SwiperSlide key={index}>
+                      <HomeHero
+                        title={news.title}
+                        body={news.body}
+                        link={news.link}
+                        target={news.target}
+                        image={news.image}
+                        imagePosition={news.imagePosition}
+                      />
+                    </SwiperSlide>
+                  )
+                })
+              }
             </Swiper>
 
             <InfoBlock
@@ -84,20 +80,20 @@ export default function HomePage() {
           <div className="space-y-40 py-40">
             <GamesCards
               title="PureCube Games"
-							image="/images/003.jpg"
+							image="/images/games-cards/001.jpg"
 							subtitle="discover, connect, play, enjoy, earn"
 							cards={[
 								{
-									isVisible: true,
+                  image: "/images/games-cards/002.png",
 								},
 								{
-									isVisible: true,
+									image: "/images/games-cards/003.png",
 								},
 								{
-									isVisible: true,
+									image: "/images/games-cards/004.png",
 								},
 								{
-									isVisible: true,
+									image: "/images/games-cards/005.png",
 								},
 							]}
             />
@@ -109,25 +105,6 @@ export default function HomePage() {
 
             <NewsList
 							title="Latest updates & news"
-							cards={[
-								{
-									title: "Discover the First Block ecosystem",
-									link: "https://pureblock.medium.com/discover-the-first-block-ecosystem-in-the-pureblock-392a7778e9ef",
-									target: "_blank",
-									image: "/images/004.png"
-								},
-								{
-									title: "All you need to know about Chubby Runners game",
-									link: "/game/chubby-runners",
-									image: "/images/006.png"
-								},
-								{
-									title: "Welcome to PureCube. The rise of blockchain gaming",
-									link: "https://medium.com/@pureblock/welcome-to-pureblock-5a78cc85738d",
-									target: "_blank",
-									image: "/images/005.png",
-								},
-							]}
 						/>
           </div>
 
