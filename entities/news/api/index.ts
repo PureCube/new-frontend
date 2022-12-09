60789f3db59339da4f8f649f453ab1a1c49efb56
KIRA <bfakhruz@gmail.com>
2022-12-09 17:22:33 +0500

    added news entity

---
+++ entities/news/api/index.ts
@@ -0,0 +1,8 @@
+export interface newsModelProps {
+	title: string,
+	body?: string,
+	link: string,
+	target?: "_self" | "_blank",
+	image?: string,
+	imagePosition?: "center" | "right" | "bottom" | "left" | "top",
+}
