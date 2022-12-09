7e3773c8712f1100f4e014ba93f72ff239817a7f
KIRA <bfakhruz@gmail.com>
2022-12-09 17:26:40 +0500

    updated home-hero ui component

--- widgets/hero/ui/home-hero/index.tsx
+++ widgets/hero/ui/home-hero/index.tsx
@@ -1,23 +1,15 @@
+import { newsModelProps } from "entities/news";
 import Image from "next/image";
 import { LinkButton } from "shared/ui";
 
-export interface HomeHeroProps {
-	title: string,
-	body: string,
-	link: string,
-	target?: "_blank" | "_self",
-	image?: string,
-	imagePosition?: "center" | "right" | "bottom" | "left" | "top",
-}
-
-export const HomeHero = ({title, body, link, target="_self", image, imagePosition="center"}: HomeHeroProps) => {
+export const HomeHero = (news: newsModelProps) => {
 	return (
 		<section className="relative overflow-hidden flex items-end h-[50vh] md:h-[80vh] bg-neutral-900 p-3 md:p-5 rounded-3xl">
-			{image && <Image src={image} layout="fill" objectFit="cover" objectPosition={imagePosition} alt="cover" />}
+			{news.image && <Image src={news.image} layout="fill" objectFit="cover" objectPosition={news.imagePosition} alt="cover" />}
 			<div className="relative w-full space-y-3">
-				<h1 className="text-3xl md:text-4xl leading-normal font-heading-semibold uppercase">{title}</h1>
-				<p className="text-base md:text-lg md:w-2/3">{body}</p>
-				<LinkButton link={link} target={target} arrow={true}>Read about</LinkButton>
+				<h1 className="text-3xl md:text-4xl leading-normal font-heading-semibold uppercase md:w-2/3">{news.title}</h1>
+				<p className="text-base md:text-lg md:w-2/3">{news.body}</p>
+				<LinkButton link={news.link} target={news.target} arrow={true}>Read about</LinkButton>
 			</div>
 		</section>
 	)
