756293911eb02ad7156d96127772093f06d70e21
KIRA <bfakhruz@gmail.com>
2022-12-09 18:01:57 +0500

    updated about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,6 +6,7 @@ import { Header } from "widgets/header";
 import { SecondHero } from "widgets/hero";
 import { Navbar } from "widgets/navbar";
 import { TeamBlock } from "widgets/team-block";
+import { teamBlockModel } from "widgets/team-block/model";
 import { ValuesBlock } from "widgets/values-block";
 
 export default function AboutPage() {
@@ -24,7 +25,7 @@ export default function AboutPage() {
         />
 
         <Main>
-          <div className="space-y-40 py-40">
+          <div className="space-y-40 pb-40">
 
             <SecondHero
               title={`Together we are \n one Pure Cube`}
@@ -55,90 +56,40 @@ export default function AboutPage() {
 
             <TeamBlock
               title="Advisors"
-              cards={[
+              cards=
                 {
-                  name: "Olga Gi",
-                  role: "Advisor, Investment Relations",
-                  image: "/images/team/002.png",
-                  linkedin: "https://www.linkedin.com/in/olga-gi-zhirova/",
-                },
-                {
-                  name: "EdK",
-                  role: "Advisor",
-                  image: "/images/team/001.png",
-                },
-              ]}
+                  teamBlockModel.advisors.map( (member) => {
+                    return (
+                      {
+                        name: member.name,
+                        role: member.role,
+                        image: member.image,
+                        linkedin: member.linkedin,
+                        github: member.github,
+                        artstation: member.artstation,
+                      }
+                    )
+                  })
+                }
             />
 
             <TeamBlock
               title="Our Team"
-              cards={[
+              cards=
                 {
-                  name: "Ziyod Bekhzodov",
-                  role: "Co-Founder, CEO",
-                  image: "/images/team/006.png",
-                  linkedin: "https://www.linkedin.com/in/bekhzodov",
-                },
-                {
-                  name: "Javokhir Nazarov",
-                  role: "Co-Founder, CTO",
-                  image: "/images/team/005.png",
-                  linkedin: "https://www.linkedin.com/in/nazarov7mu/",
-                },
-                {
-                  name: "Alien Deployer",
-                  role: "Co-Founder, Tech Lead",
-                  image: "/images/team/003.png",
-                  github: "https://github.com/a17",
-                },
-                {
-                  name: "Fozilbek Bekhzodov",
-                  role: "Co-Founder, Game Producer",
-                  image: "/images/team/004.png",
-                  linkedin: "https://www.linkedin.com/in/bekhzodov7mu/",
-                  github: "https://github.com/bekhzodov7mu",
-                },
-                {
-                  name: "Mukhammad Botirov",
-                  role: "Co-Founder, 3D Team Lead",
-                  image: "/images/team/001.png",
-                  artstation: "https://www.artstation.com/maxmax17",
-                },
-                {
-                  name: "Denis Kan",
-                  role: "Unity Developer",
-                  image: "/images/team/008.png",
-                  linkedin: "https://www.linkedin.com/in/denis-kan-3b86b0166",
-                },
-                {
-                  name: "Fakhruz Bakhromov",
-                  role: "Front-end Developer",
-                  image: "/images/team/001.png",
-                  linkedin: "https://www.linkedin.com/in/kirademiurge/",
-                  github: "https://github.com/kirademiurge",
-                },
-                {
-                  name: "Vladislav Plufix",
-                  role: "Community Manager",
-                  image: "/images/team/003.png",
-                },
-                {
-                  name: "Diana Khay",
-                  role: "2D Artist",
-                  image: "/images/team/002.png",
-                },
-                {
-                  name: "Michael Pavlychev",
-                  role: "3D Artist",
-                  image: "/images/team/005.png",
-                },
-                {
-                  name: "Mansur Vasitov",
-                  role: "Unity Developer",
-                  image: "/images/team/004.png",
-                  linkedin: "https://www.linkedin.com/in/mansur-vasitov/"
-                },
-              ]}
+                  teamBlockModel.ourTeam.map( (member) => {
+                    return (
+                      {
+                        name: member.name,
+                        role: member.role,
+                        image: member.image,
+                        linkedin: member.linkedin,
+                        github: member.github,
+                        artstation: member.artstation,
+                      }
+                    )
+                  })
+                }
               others={true}
             />
 
