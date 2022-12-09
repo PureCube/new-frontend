f90ca2629efcd505f5b4bab59bf9cea437dd5b81
KIRA <bfakhruz@gmail.com>
2022-12-09 19:07:38 +0500

    updated news-list widget

--- widgets/news-list/index.tsx
+++ widgets/news-list/index.tsx
@@ -1,17 +1,12 @@
-import { NewsCard } from "./ui/news-card";
+import { NewsCard } from "entities/news";
 import { LinkButton } from "shared/ui";
-import { newsModelProps } from "entities/news";
 import { newsListModel } from "./model";
 
-export interface NewsListProps {
-	title: string,
-}
-
-export const NewsList = ({title}: NewsListProps) => {
+export const NewsList = () => {
 	return (
 		<section className="bg-neutral-900 p-3 md:p-5 rounded-3xl space-y-3 md:space-y-5">
 			<div className="flex items-center justify-between gap-3 md:gap-5">
-				<h3 className="text-2xl md:text-3xl font-heading-semibold">{title}</h3>
+				<h3 className="text-2xl md:text-3xl font-heading-semibold">Latest updates & news</h3>
 				<LinkButton link="/" arrow={true} type="secondary">show all</LinkButton>
 			</div>
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-3 md:gap-5">
