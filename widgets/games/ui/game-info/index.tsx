a493d1adf34c9c6f6f1ca11713ffb1043143ab18
KIRA <bfakhruz@gmail.com>
2022-12-09 17:36:38 +0500

    updated game-info ui component

--- widgets/games/ui/game-info/index.tsx
+++ widgets/games/ui/game-info/index.tsx
@@ -1,20 +1,20 @@
 import { gameModelProps } from "entities/game";
 import Image from "next/image";
 
-export const GameInfo = ({cover, about, title, developer, platform, genre, status, network, engine, download}: gameModelProps) => {
+export const GameInfo = (game: gameModelProps) => {
 	return (
 		<section className="flex flex-col gap-5">
-			<h3 className="text-xl md:text-2xl font-heading-semibold uppercase">{title}</h3>
+			<h3 className="text-xl md:text-2xl font-heading-semibold uppercase">{game.title}</h3>
 
 			<div className="flex flex-col md:flex-row gap-20 md:gap-5">
 
 				<div className="md:w-8/12 space-y-3 md:space-y-5">
 					<div className="relative overflow-hidden pt-[66%] md:pt-[56%] bg-neutral-900 rounded-3xl">
-						{cover && <Image src={cover} layout="fill" objectFit="cover" objectPosition="bottom" alt="cover" />}
+						{game.cover && <Image src={game.cover} layout="fill" objectFit="cover" objectPosition="bottom" alt="cover" />}
 					</div>
-					<div className={`bg-neutral-900 p-3 md:p-5 rounded-3xl space-y-3 md:space-y-5 ${!about && "hidden"}`}>
+					<div className={`bg-neutral-900 p-3 md:p-5 rounded-3xl space-y-3 md:space-y-5 ${!game.about && "hidden"}`}>
 						{
-							about && about.map( (el) => {
+							game.about && game.about.map( (el) => {
 								return (
 									<>
 										<h3 className="text-2xl md:text-3xl font-heading-semibold uppercase">{el.title}</h3>
@@ -30,11 +30,11 @@ export const GameInfo = ({cover, about, title, developer, platform, genre, statu
 					<ul className="bg-neutral-900 p-3 md:p-5 rounded-3xl space-y-5 font-body-semibold uppercase text-base md:text-lg">
 						<li className="flex justify-between items-center">
 							<span>Platform: </span>
-							<span className="bg-neutral-800 rounded-lg px-2 pt-1">{platform}</span>
+							<span className="bg-neutral-800 rounded-lg px-2 pt-1">{game.platform}</span>
 						</li>
 						<li className="flex justify-between items-center">
 							<span>Genre: </span>
-							<span className="bg-neutral-800 rounded-lg px-2 pt-1">{genre}</span>
+							<span className="bg-neutral-800 rounded-lg px-2 pt-1">{game.genre}</span>
 						</li>
 						<li className="flex justify-between items-center">
 							<span>Status: </span>
@@ -43,21 +43,21 @@ export const GameInfo = ({cover, about, title, developer, platform, genre, statu
 						<li className="flex justify-between items-center">
 							<span>Network: </span>
 							<span className="bg-neutral-800 rounded-lg px-2 py-1">
-								{network === "near" && <img src="/icons/near2.svg" alt="icon" />}
-								{network !== "near" && network}
+								{game.network === "near" && <img src="/icons/near2.svg" alt="icon" />}
+								{game.network !== "near" && game.network}
 								</span>
 						</li>
 						<li className="flex justify-between items-center">
 							<span>Engine: </span>
 							<span className="bg-neutral-800 rounded-lg px-2 py-1">
-								{engine === "unity" && <img src="/icons/unity.svg" alt="icon" />}
-								{engine !== "unity" && engine}
+								{game.engine === "unity" && <img src="/icons/unity.svg" alt="icon" />}
+								{game.engine !== "unity" && game.engine}
 							</span>
 						</li>
-						{download && download.hasOwnProperty("appstore") && download.hasOwnProperty("googleplay") &&
+						{game.download && game.download.hasOwnProperty("appstore") && game.download.hasOwnProperty("googleplay") &&
 						<li className="flex gap-3 md:gap-5">
-							<a href={download.appstore} target="_blank" rel="noreferrer"><img src="/icons/appstore.svg" alt="link" /></a>
-							<a href={download.googleplay} target="_blank" rel="noreferrer"><img src="/icons/googleplay.svg" alt="link" /></a>
+							<a href={game.download.appstore} target="_blank" rel="noreferrer"><img src="/icons/appstore.svg" alt="link" /></a>
+							<a href={game.download.googleplay} target="_blank" rel="noreferrer"><img src="/icons/googleplay.svg" alt="link" /></a>
 						</li>
 						}
 					</ul>
