6eb3c600dbb1a9c8eefcbbc97433bec7aeae4ad3
KIRA <bfakhruz@gmail.com>
2022-12-09 17:28:07 +0500

    updated news-card ui component

--- widgets/news-list/ui/news-card/index.tsx
+++ widgets/news-list/ui/news-card/index.tsx
@@ -1,22 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
+import { newsModelProps } from "entities/news";
 
-export interface NewsCardProps {
-	title: string,
-	link: string,
-	target?: "_self" | "_blank",
-	image?: string,
-}
-
-export const NewsCard = ({title, link, target="_self", image}: NewsCardProps) => {
+export const NewsCard = (news: newsModelProps) => {
 	return (
-		<a href={link} target={target} className="bg-neutral-800 rounded-3xl overflow-hidden">
+		<a href={news.link} target={news.target} className="bg-neutral-800 rounded-3xl overflow-hidden">
 			<div className="relative overflow-hidden bg-neutral-900 rounded-3xl pt-[56%] sm:pt-[40%] lg:pt-[66%]">
-				{image && <Image src={image} layout="fill" objectFit="cover" objectPosition="bottom" alt="cover" />}
+				{news.image && <Image src={news.image} layout="fill" objectFit="cover" objectPosition="bottom" alt="cover" />}
 			</div>
 			<p className="relative text-base md:text-lg uppercase font-heading-semibold p-3 pr-9 md:p-5 md:pr-14">
-				{title}
+				{news.title}
 				<FontAwesomeIcon className='w-5 absolute top-3 md:top-5 right-3 md:right-5' icon={faArrowUpRightFromSquare} />
 			</p>
 		</a>
