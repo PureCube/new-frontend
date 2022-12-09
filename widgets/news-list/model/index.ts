01063494e7220432d5d189cd5fdc0cb58df83b9b
KIRA <bfakhruz@gmail.com>
2022-12-09 17:23:04 +0500

    added news-list widget model

---
+++ widgets/news-list/model/index.ts
@@ -0,0 +1,32 @@
+import { newsModelProps } from "entities/news";
+import {makeAutoObservable} from 'mobx';
+
+export interface newsListModelProps {
+	newsList: newsModelProps[],
+}
+
+export const newsListModel: newsListModelProps = makeAutoObservable({
+	// STORE
+	newsList: [
+		{
+			title: "Welcome to PureCube. The rise of blockchain gaming",
+			body: "PureCube is a next-generation gaming studio launching its games and developing a unified gaming platform for blockchain games",
+			link: "https://medium.com/@pureblock/welcome-to-pureblock-5a78cc85738d",
+			target: "_blank",
+			image: "/images/001.jpg",
+			imagePosition: "right",
+		},
+		{
+			title: "Discover the First Block ecosystem in the PureCube",
+			link: "https://pureblock.medium.com/discover-the-first-block-ecosystem-in-the-pureblock-392a7778e9ef",
+			target: "_blank",
+			image: "/images/games/near-runner.jpg",
+			imagePosition: "bottom",
+		},
+		{
+			title: "All you need to know about Chubby Runners game",
+			link: "/game/chubby-runners",
+			image: "/images/games/chubby-runners.png",
+		},
+	]
+});
