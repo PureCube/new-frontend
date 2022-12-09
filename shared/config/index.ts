b8471c06c1c80a6680522132abe5ef191d2613e8
KIRA <bfakhruz@gmail.com>
2022-12-09 18:23:53 +0500

    updated shared config

--- shared/config/index.ts
+++ shared/config/index.ts
@@ -1,3 +1,10 @@
 export const config = {
 	projectName: "PureCube",
+	twitter: "https://twitter.com/purecube_io",
+	medium: "https://medium.com/@pureblock",
+	discord: "https://discord.gg/KDe48PgnE5",
+	email: "hello@purecube.io",
+	github: "https://github.com/purecube",
+	telegram: "https://t.me/PureCube",
+	mediaKit: "https://drive.google.com/drive/folders/1GEAxFFx19MeGGVdqpw-sR7zRhaHs1_iy?usp=sharing",
 }
