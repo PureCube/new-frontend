66eb5bde466c59eb36ac18212accf7d01dc8ae52
KIRA <bfakhruz@gmail.com>
2022-12-12 17:04:40 +0500

    updated uikit button

--- shared/ui/button/index.tsx
+++ shared/ui/button/index.tsx
@@ -1,9 +1,27 @@
 export interface ButtonProps {
 	children: string,
+	type?: "primary" | "secondary",
+	onClick?: any,
 }
 
-export const Button = ({children}: ButtonProps) => {
-	return (
-		<button className="font-heading-semibold text-sm md:text-base text-black bg-white rounded-full px-5 py-2 select-none">{children}</button>
+export const Button = ({children, type="primary", onClick}: ButtonProps) => {
+	if (type === "primary") return (
+		<button
+			className="font-heading-semibold text-sm md:text-base text-black bg-white rounded-full px-5 py-2 select-none"
+			onClick={onClick}
+			>
+				{children}
+		</button>
 	)
+
+	if (type === "secondary") return (
+		<button
+			className="font-heading-semibold text-sm md:text-base text-white bg-neutral-800 rounded-full px-5 py-2 select-none"
+			onClick={onClick}
+			>
+				{children}
+		</button>
+	)
+
+	else return null;
 }
