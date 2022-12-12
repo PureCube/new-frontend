7171bd40996fca031b3eafe709bf3ef86ce1b146
KIRA <bfakhruz@gmail.com>
2022-12-12 19:47:31 +0500

    added register-form ui component

--- entities/auth/ui/register-form/index.tsx
+++ entities/auth/ui/register-form/index.tsx
@@ -1,13 +1,34 @@
+import { userModel } from "entities/user";
 import { useState } from "react";
-import { Button, Input } from "shared/ui"
+import { Button, Input } from "shared/ui";
 
-export const RegisterForm = () => {
+export interface RegisterFormProps {
+	isOpened: boolean,
+	setIsOpened: any;
+}
+
+export const RegisterForm = ({isOpened=false, setIsOpened}: RegisterFormProps) => {
 	const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
   const [nameInput, setNameInput] = useState("");
 
+	const toggleModal = () => {
+		setIsOpened(!isOpened);
+	}
+
+	const registerHandler = (event: any) => {
+    event.preventDefault();
+    if (emailInput !== "" && passwordInput !== "" && nameInput !== "") {
+      userModel.registerUser(emailInput, passwordInput, nameInput);
+      setEmailInput("");
+      setPasswordInput("");
+      setNameInput("");
+      setIsOpened(!isOpened);
+    }
+  }
+
 	return (
-		<form className="flex flex-col gap-3">
+		<form onSubmit={ (event) => event.preventDefault() } className="flex flex-col gap-3">
 			<h3 className="text-xl md:text-2xl font-heading-semibold">Create account</h3>
 			<Input
 					type="text"
@@ -28,8 +49,8 @@ export const RegisterForm = () => {
           onChange={ (event: any) => setPasswordInput(event.target.value) }
         />
 				<div className="pt-3 space-x-3">
-					<Button>Submit</Button>
-					<Button type="secondary">cancel</Button>
+					<Button onClick={registerHandler}>Submit</Button>
+					<Button onClick={toggleModal} type="secondary">cancel</Button>
 				</div>
 		</form>
 	)
