cf2dab0cf6b1d7f31376dbfa75a7765c040834d7
KIRA <bfakhruz@gmail.com>
2022-12-12 19:44:41 +0500

    added user-profile-card ui component

---
+++ entities/user/ui/user-profile-card/index.tsx
@@ -0,0 +1,9 @@
+export const UserProfileCard = () => {
+	return (
+		<div className="relative pt-[50%] lg:pt-[56%] xl:pt-[50%] bg-neutral-900 rounded-3xl">
+			<div className="absolute top-0 bottom-0 p-3 md:p-5">
+					kekekekek
+				</div>
+		</div>
+	)
+}
