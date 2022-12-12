b74e7455aadff3e418404a393191a3c8ad914a78
KIRA <bfakhruz@gmail.com>
2022-12-12 17:07:02 +0500

    added uikit modal

---
+++ shared/ui/modal/index.tsx
@@ -0,0 +1,32 @@
+import { Children, useState } from "react";
+import { Button } from "../button";
+import { Container } from "../container";
+
+export interface ModalProps {
+	children: string | JSX.Element | JSX.Element[],
+	isOpened: boolean,
+	setIsOpened: any;
+	onSubmit?: any,
+}
+
+export const Modal = ({children, isOpened=false, setIsOpened, onSubmit}: ModalProps) => {
+
+	const toggleModal = () => {
+		setIsOpened(!isOpened);
+	}
+
+	 if (isOpened) return (
+		<div onClick={toggleModal} className="fixed z-30 top-0 bottom-0 right-0 left-0 bg-neutral-900/50 flex justify-center items-center">
+
+			<Container>
+				<div onClick={ (event) => event.stopPropagation() } className="mx-auto bg-neutral-900 rounded-3xl p-3 md:p-5 w-full lg:w-1/2">
+
+					{children}
+
+				</div>
+			</Container>
+
+		</div>
+	)
+	else return null;
+}
