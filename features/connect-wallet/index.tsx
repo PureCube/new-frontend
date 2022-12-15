70221e89b10c280c3bc816a0c805b53ffb2eb234
KIRA <bfakhruz@gmail.com>
2022-12-15 16:55:54 +0500

    updated connect-wallet feature

--- features/connect-wallet/index.tsx
+++ features/connect-wallet/index.tsx
@@ -1,11 +1,39 @@
+import { web3Store } from "entities/wallet";
+import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
 import { Button } from "shared/ui";
+import {Account} from "@near-wallet-selector/core";
+import "@near-wallet-selector/modal-ui/styles.css";
 
 export interface ConnectWalletProps {
 	children: string,
 }
 
-export const ConnectWallet = ({children}: ConnectWalletProps) => {
+export const ConnectWallet = observer( ({children}: ConnectWalletProps) => {
+	const [walletModal, setWalletModal] = useState(false)
+  const [account, setAccount] = useState<Account>()
+
+  useEffect(() => {
+    const a = async() => {
+      await web3Store.init()
+      if (web3Store.account) {
+        setAccount(web3Store.account)
+      }
+    }
+    a()
+  }, [])
+
+  const connectWallet = () => {
+    web3Store.modal?.show()
+  }
+
+  const disconnectWallet = async () => {
+    await web3Store.disconnect()
+    setWalletModal(false)
+    setAccount(undefined)
+  }
+
 	return (
-		<Button>{children}</Button>
+		<Button onClick={connectWallet}>{children}</Button>
 	)
-}
+})
